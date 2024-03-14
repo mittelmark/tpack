ea5e2a979e7b83998ad2a5c40c21d4f996cac983
Detlef Groth <dgroth@uni-potsdam.de>
2024-03-14 12:40:51 +0100

    adding mini example files

---
+++ sample/mini.vfs/lib/test/pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded test 0.1 [list source [file join $dir test.tcl]]
+
---
+++ sample/mini.vfs/lib/test/test.tcl
@@ -0,0 +1,4 @@
+package require Tcl
+package provide test 0.1
+namespace eval ::test { }
+proc ::test::hello { } { puts "Hello World!" }
